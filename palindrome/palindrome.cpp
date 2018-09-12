24d681684b9d585f1f7ecd6548a532f20d1ac10d
AlexGitHub99 <351013@bsd48.org>
2018-09-12 11:20:15 -0700

    Program now attemps to remove non letters and change capitals to lowercase while copying the input into a new string. It fails and prints random letters. The reversing part was changed then commented out

--- palindrome/palindrome.cpp
+++ palindrome/palindrome.cpp
@@ -4,20 +4,43 @@
 using namespace std;
 
 main() {
-  char string[81];
+  char input[81];
   char rvString[81];
-  
   while(true) {
     cout << "Please enter no more than 80 characters of text";
-    cin.get(string, 81);
+    cin.get(input, 81);
     while(cin.get() != '\n');
-    for(int i = 0; i < 81; i ++) {
-      if(string[i] != '\0') {
-	rvString[80 - i] = string[i];
+    char string[strlen(input)];
+    int j = 0;
+    for(int i = 0; i < 81; i++) {
+      if(input[i] >= 65 && input[i] <= 90) { // it's a capital letter 
+	string[j] = input[i] + 32; //converts the letter into a lowercase before copying it over into string
+	j++;
+      } else if (input[i] >= 97 && input[i] <= 122) { //it's a lowercase letter
+	string[j] = input[i];
+	j++;
+      } else if (input[i] == '\0') { // it's a null terminating character
+	string[j] = input[i];
       }
     }
+    //writing = false;
+    //int j = 0;
+    //for(int i = 0; i < 81; i++) {
+    //if(writing == true && string[80 - i] !=  ) {
+    //	rvString[j] = string[80 - i];
+    //	j++
+    //}
+    //if(string[80 - i] == '\0') {
+    //	writing = true;
+    //	
+    //}
+    //if(writing == true) {
+    //	rvString[j] = string[80 - i - 1];
+    //	j++
+    //}
     cout << string << "\n";
-    cout << rvString << "\n";
+    
+    //cout << rvString << "\n";
 		      
   }
 
