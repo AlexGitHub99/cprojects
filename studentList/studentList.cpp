bf04e91b8c6d308453071f00e4d449d41f32c7d3
AlexGitHub99 <351013@bsd48.org>
2018-09-28 12:27:54 -0700

    Fixed errors so the vector pointer of struct pointers is created correctly and started changing the functions to take arguements correctly

--- studentList/studentList.cpp
+++ studentList/studentList.cpp
@@ -1,26 +1,23 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+
 using namespace std;
 
-void addStudent(char first[10], char second[10], int id, float gpa);
+void addStudent(&vector<student*>*, char first[10], char second[10], int id, float gpa);
 void printStudent();
 void delStudent(int id);
 
-main() {
-  
-  struct student {
-    char first[10];
-    char second[10];
-    int id;
-    float gpa;
-  }
-  
-  vector<*int> studentList;
-  int *vectorPtr = &studentList;
-  cout << vectorPtr << endl;
-  
+struct student {
+  char first[10];
+  char second[10];
+  int id;
+  float gpa;
+};
 
-  
+main() {
+ 
+  vector<student*>* studentList = new vector<student*>(); //creates a new vector pointer of struct pointers on the heap
+  cout << studentList;
   
 }
