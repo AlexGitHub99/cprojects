1878decb4a33c05537e11fd74cdb9c52b853b624
AlexGitHub99 <351013@bsd48.org>
2018-10-02 12:27:25 -0700

    fix the function arguements and made addStudent create a new student struct and assign the passed in values to it.

--- studentList/studentList.cpp
+++ studentList/studentList.cpp
@@ -4,20 +4,43 @@
 
 using namespace std;
 
-void addStudent(&vector<student*>*, char first[10], char second[10], int id, float gpa);
-void printStudent();
-void delStudent(int id);
-
 struct student {
   char first[10];
-  char second[10];
+  char last[10];
   int id;
   float gpa;
 };
 
-main() {
+void addStudent(vector<student*>* &studentList, char first[10], char last[10], int id, float gpa);
+void printStudent(vector<student*>* &studentList);
+void delStudent(vector<student*>* &studentList, int id);
+
+int main() {
  
   vector<student*>* studentList = new vector<student*>(); //creates a new vector pointer of struct pointers on the heap
-  cout << studentList;
+  cout << studentList << endl;
+  addStudent(studentList, "Alex", "King", 351013, 4.218);
+  addStudent(studentList, "Michael", "Kong", 32134823, 1.0);
+}
+
+void addStudent(vector<student*>* &studentList, char first[10], char last[10], int id, float gpa) {
+  student* newStudent = new student;
+
+  for(int i = 0; i < 10; i++ ) { //sets the first name of the student struct to the passed in value
+    newStudent->first[i] = first[i];
+  }
+
+  for(int i = 0; i < 10; i ++) { //sets the last name of the student struct to the passed in value
+    newStudent->last[i] = last[i];
+  }
+
+  newStudent->id = id;
+  newStudent->gpa = gpa;
+  
+  cout << newStudent->first << endl;
+  cout << newStudent->last << endl;
+  cout << newStudent->id << endl;
+  cout << newStudent->gpa << endl;
+  
   
 }
