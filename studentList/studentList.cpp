ab6d173ce354d96856ef303e250a596fdbc434f0
AlexGitHub99 <351013@bsd48.org>
2018-10-04 12:28:52 -0700

    made addStudent function work without error and added printStudent, which works but doesn't print GPAs to exactly 2 decimals yet. Added part of delStudent

--- studentList/studentList.cpp
+++ studentList/studentList.cpp
@@ -18,29 +18,53 @@ void delStudent(vector<student*>* &studentList, int id);
 int main() {
  
   vector<student*>* studentList = new vector<student*>(); //creates a new vector pointer of struct pointers on the heap
-  cout << studentList << endl;
   addStudent(studentList, "Alex", "King", 351013, 4.218);
   addStudent(studentList, "Michael", "Kong", 32134823, 1.0);
+  printStudent(studentList);
 }
 
 void addStudent(vector<student*>* &studentList, char first[10], char last[10], int id, float gpa) {
   student* newStudent = new student;
-
+  
   for(int i = 0; i < 10; i++ ) { //sets the first name of the student struct to the passed in value
     newStudent->first[i] = first[i];
   }
-
+  
   for(int i = 0; i < 10; i ++) { //sets the last name of the student struct to the passed in value
     newStudent->last[i] = last[i];
   }
-
+ 
   newStudent->id = id;
   newStudent->gpa = gpa;
+
+  //vector<student*>::iterator it = studentList->begin();
+
+  studentList->push_back (newStudent); //inserts newStudent after the last element in studentList
+
+  /***
+  cout << studentList << endl;
   
   cout << newStudent->first << endl;
   cout << newStudent->last << endl;
   cout << newStudent->id << endl;
   cout << newStudent->gpa << endl;
+  ***/
   
-  
+}
+
+void printStudent(vector<student*>* &studentList) {
+  for(int i = 0; i < studentList->size(); i++) {
+    cout << (*studentList)[i]->first << " " << (*studentList)[i]->last << ", ";
+    cout << (*studentList)[i]->id << ", ";
+    cout << (*studentList)[i]->gpa << ", ";
+  }
+  cout << "this jsut ran" << endl;
+}
+
+void delStudent(vector<student*>* &studentList, int id) {
+  for(int i = 0; i < studentList->size(); i++) {
+    if((*studentList)[i]->id == id) {
+      
+    } 
+  }
 }
