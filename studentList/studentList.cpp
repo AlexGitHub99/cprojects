1be254e88c94e739f5803f9f3c24470437ee828b
AlexGitHub99 <351013@bsd48.org>
2018-10-08 12:29:11 -0700

    Made the delete function work correctly and started implementing user input and commands, but is currently unfinished and has an error.

--- studentList/studentList.cpp
+++ studentList/studentList.cpp
@@ -11,18 +11,38 @@ struct student {
   float gpa;
 };
 
-void addStudent(vector<student*>* &studentList, char first[10], char last[10], int id, float gpa);
+void addStudent(vector<student*>* &studentList, char first[11], char last[11], int id, float gpa);
 void printStudent(vector<student*>* &studentList);
 void delStudent(vector<student*>* &studentList, int id);
 
 int main() {
  
   vector<student*>* studentList = new vector<student*>(); //creates a new vector pointer of struct pointers on the heap
+
+  cout << "Welcome to Student List! Commands: add, print, delete.";
+  
+  bool running = true;
+  
+  while(running == true) { // main loop
+    char input[10]; 
+    cin.get(input, 7);
+    while(cin.get() != '\n');
+    if(strcmp(input, "add") == 0) { //user typed add command
+
+      cout << "Please enter the students first name (10 letters or less)";
+      cin.get(char first[11], 11);	
+      while(cin.get() != '\n');
+      
+    }
+  }
+  /***
   addStudent(studentList, "Alex", "King", 351013, 4.218);
   addStudent(studentList, "Michael", "Kong", 32134823, 1.0);
   printStudent(studentList);
+  delStudent(studentList, 351013);
+  printStudent(studentList);
+  ***/
 }
-
 void addStudent(vector<student*>* &studentList, char first[10], char last[10], int id, float gpa) {
   student* newStudent = new student;
   
@@ -56,15 +76,16 @@ void printStudent(vector<student*>* &studentList) {
   for(int i = 0; i < studentList->size(); i++) {
     cout << (*studentList)[i]->first << " " << (*studentList)[i]->last << ", ";
     cout << (*studentList)[i]->id << ", ";
-    cout << (*studentList)[i]->gpa << ", ";
+    cout << (*studentList)[i]->gpa << endl;
   }
-  cout << "this jsut ran" << endl;
+  cout << "this just ran" << endl;
 }
 
 void delStudent(vector<student*>* &studentList, int id) {
   for(int i = 0; i < studentList->size(); i++) {
     if((*studentList)[i]->id == id) {
-      
+      delete[] (*studentList)[i];
+      studentList->erase (studentList->begin() + i);
     } 
   }
 }
