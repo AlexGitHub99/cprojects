d169bed1eea95f490d9499aa48b13d1a6250ea79
AlexGitHub99 <351013@bsd48.org>
2018-10-09 21:37:41 -0700

    Added more user input functionality and fixed the previous errors.[C

--- studentList/studentList.cpp
+++ studentList/studentList.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
-
+#include <cmath>
 using namespace std;
 
 struct student {
@@ -29,9 +29,32 @@ int main() {
     while(cin.get() != '\n');
     if(strcmp(input, "add") == 0) { //user typed add command
 
-      cout << "Please enter the students first name (10 letters or less)";
-      cin.get(char first[11], 11);	
+      char first[11];
+      char last[11];
+      int id = 0;
+      float gpa;
+      
+      cout << "Please enter the students first name (10 letters or less)";   
+      cin.get(first, 11);	
       while(cin.get() != '\n');
+
+      cout << "Please enter the student last name (10 letters or less)";
+      cin.get(last, 11);
+      while(cin.get() != '\n');
+
+      cout << "Please enter the students id (6 numbers)";
+      char idText[7];
+      cin.get(idText, 7);
+      while(cin.get() != '\n');
+      for(int i = 0; i < 6; i++) {
+	//Friend: Nathan Class: C++ programming 6
+	//I am using their idea with their permission
+	//Their idea was to combine the integer values of each character of the id into a single integer by multiplying each by 10 to the power of it's place value, then adding them
+	id += (idText[i] - 48) * pow (10, 5 - i);
+	//end of code where idea was used
+      }
+      cout << id;
+
       
     }
   }
