35c3ef52086b8e57f4e4a3aea67bab4581884b12
AlexGitHub99 <351013@bsd48.org>
2018-12-08 18:33:29 -0800

    changed grade field of student header file to id

--- linkedList/Student.h
+++ linkedList/Student.h
@@ -1,16 +1,16 @@
-//Student class of linked list. Cohtains student name, grade, and gpa fields, and getters and setters
+//Student class of linked list. Cohtains student name, id, and gpa fields, and getters and setters
 #include <iostream>
 
 class Student {
  public:
   void setName(char* name);
-  void setGrade(int grade);
+  void setid(int id);
   void setGpa(float gpa);
   char* getName();
   int getGrade();
   void getGpa();
  private:
   char* name;
-  int grade;
+  int id;
   float gpa;
 };
