fdb92bbf0eb4ff3199f64d45c1ec75e4ba9eb61e
AlexGitHub99 <351013@bsd48.org>
2018-12-18 12:30:43 -0800

    coppied over studentList.cpp from the studentList project and started changing it to work with nodes. There are erors

--- linkedListPart1/Main.cpp
+++ linkedListPart1/Main.cpp
@@ -1,9 +1,13 @@
+//Main class, used to test node and student functionality
+
 #include  "Node.h"
 #include "Student.h"
+#include <iostream>
 
 using namespace std;
 
 int main () {
+  //Create two new students and set their data
   Student* student1 = new Student();
   student1->setName("Joe");
   student1->setId(351013);
@@ -13,8 +17,11 @@ int main () {
   student2->setId(39549);
   student2->setGpa(1.00);
   
+  //Create a head node with the first student
   Node* node1 = new Node(student1);
+  //add the next node with the second student
   node1->setNext(new Node(student2));
+  //Print out the value of each student using the nodes
   cout << "Student of Node 1:" << endl;
   cout << node1->getValue()->getName() << " ";
   cout << node1->getValue()->getId() << endl;
