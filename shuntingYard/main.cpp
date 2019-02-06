5812ba894c32f71e06a99be273dae85152097552
Alex King <kingalex2002@gmail.com>
2019-02-06 11:21:08 -0800

    Added an input prompt and some of the algorithm for converting it into postfix.

--- shuntingYard/main.cpp
+++ shuntingYard/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "Node.h"
-using namesapce strd;
+#include <stack>
+
+using namesapce std;
 
 main () {
-	
+	stack down;
+	stack right;
+	cout << "Please enter a series of operations." << endl;
+	cin.get(char* cChar, 10, ' ');
+	if(cChar > 48 && cChar < 57 || cChar == '(') { // char is a number
+		right.push(cChar);
+	}
 }
+
