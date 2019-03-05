9db1c42be708a9c280cf7c2d58a047383cecb4b7
U-BSD\351013 <351013@SHS-10D9C06DD.beaverton.k12.or.us>
2019-03-05 12:25:42 -0800

    Created more functionality for user input and inserting the number into the array, but there are still errors

--- heap/main.cpp
+++ heap/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
-#include "Node.h"
 #include <cstring>
+#include <cmath>
 
 //student: Zareef
 //Idea for storing binary tree as array
 
 using namespace std;
 
-int main {
+int main () {
+	int tree[1000];
 	char input[11];
-	cout << "Would you like to enter numbers as console input or a file name? Type 'input' or 'file'"
+	cout << "Would you like to enter numbers as console input or a file name? Type 'input' or 'file'";
 	cin.get(input, 10, '\n');
+	while (cin.get() != '\n');
 	if (strcmp(input, "input") == 0) {
 		cout << "Enter up to 100 numbers one at a time";
+		cin.get(input, 10, '\n');
+		while (cin.get() != '\n');
+		int number = 0;
+		for (int i = 0; i < strlen(input);  i++) {
+			number+= (input[i] - 48) * pow(10, strlen(input) - i - 1);
+		}
+		for (int i = 0; i sizeOf(tree); i++) {
+			if (number > tree[i] || tree[i] == 0) {
+				int temp = tree[i];
+				tree[i] = number;
+				number = temp;
+			}
 	}
 
 }
