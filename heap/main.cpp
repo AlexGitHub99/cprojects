fdaf8c0a9d8bc43eaa1c2b1ec5671207f912f296
AlexGitHub99 <351013@bsd48.org>
2019-03-11 23:13:49 -0700

    Final version, finished file input functionality and cleaned up UI

--- heap/main.cpp
+++ heap/main.cpp
@@ -11,41 +11,72 @@ using namespace std;
 void print(int tree[100]);
 
 int main () {
-	int tree[100];
-	for(int i = 0; i < 100; i++) {
-	  tree[i] = -1;
+  int tree[100];
+  for(int i = 0; i < 100; i++) {
+    tree[i] = -1;
+  }
+  
+  char input[21];
+  cout << "Would you like to enter numbers as console input or a file name? Type 'input' or 'file': ";
+  cin.get(input, 10, '\n');
+  while (cin.get() != '\n');
+  if (strcmp(input, "input") == 0) {
+    cout << "Enter up to 100 numbers, one at a time: ";
+    while (true) {
+      cin.get(input, 10, '\n');
+      while (cin.get() != '\n');
+      int number = 0;
+      for (int i = 0; i < strlen(input); i++) {
+	number += (input[i] - 48) * pow(10, strlen(input) - i - 1);
+      }
+      for (int i = 0; i < 100; i++) {
+	if (number > tree[i] || tree[i] == 0) {
+	  int temp = tree[i];
+	  tree[i] = number;
+	  number = temp;
 	}
-	
-	char input[11];
-	cout << "Would you like to enter numbers as console input or a file name? Type 'input' or 'file'";
-	cin.get(input, 10, '\n');
-	while (cin.get() != '\n');
-	if (strcmp(input, "input") == 0) {
-		cout << "Enter up to 100 numbers one at a time";
-		while (true) {
-			cin.get(input, 10, '\n');
-			while (cin.get() != '\n');
-			int number = 0;
-			for (int i = 0; i < strlen(input); i++) {
-				number += (input[i] - 48) * pow(10, strlen(input) - i - 1);
-			}
-			for (int i = 0; i < 100; i++) {
-				if (number > tree[i] || tree[i] == 0) {
-					int temp = tree[i];
-					tree[i] = number;
-					number = temp;
-				}
-			}
-			print(tree);
-		}
-	} else if (strcmp(input, "file") == 0) {
-	  cout << "Enter the file name";
-	  cin.get(input, 10,'\n');
-	  while(cin.get() != '\n');
-	  ofstream newFile;
-	  newFile.open(input);
-	  
+      }
+      print(tree);
+      cout << endl;
+      int i = 0;
+      while(tree[i] != -1 and i < 100) {
+	cout << tree[i] << ", ";
+	i++;
+      }
+      cout << endl;
+    }
+  } else if (strcmp(input, "file") == 0) {
+    cout << "Enter the file name: ";
+    cin.get(input, 20,'\n');
+    while(cin.get() != '\n');
+    ifstream newFile(input);
+    while(true) {
+      newFile.get(input, 4, ' ');
+      newFile.get();
+      if(strlen(input) == 0) {
+	break;
+      }
+      int number = 0;
+      for (int i = 0; i < strlen(input); i++) {
+	number += (input[i] - 48) * pow(10, strlen(input) - i - 1);
+      }
+      for (int i = 0; i < 100; i++) {
+	if (number > tree[i] || tree[i] == 0) {
+	  int temp = tree[i];
+	  tree[i] = number;
+	  number = temp;
 	}
+      }
+    }
+    print(tree);
+    cout << endl;
+    int i = 0;
+    while(tree[i] != -1 and i < 100) {
+      cout << tree[i] << ", ";
+      i++;
+    }
+  }
+  cout << endl;
 }
 
 void print(int tree[100]) {
