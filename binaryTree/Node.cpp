445e34f8e24ccce5b4779ba7daa71c6c608fe6c0
U-BSD\351013 <351013@SHS-10D9C06DD.beaverton.k12.or.us>
2019-03-15 12:27:18 -0700

    Created some of Node.cpp, some errors

--- binaryTree/Node.cpp
+++ binaryTree/Node.cpp
@@ -1,1 +1,24 @@
 #include "Node.h"
+
+Node::Node() {
+  right = NULL;
+  left = NULL;
+  data = NULL;
+}
+
+Node::getRight() {
+  return right;
+}
+
+Node::getLeft() {
+  return left;
+}
+
+Node::getData() {
+  return data;
+}
+
+Node::setRight(Node* newRight) {
+  right = newRight;
+}
+
--- binaryTree/main.cpp
+++ binaryTree/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Node.h"
 
 using namespace std;
 
