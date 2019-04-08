0693c35d3c1c54ad40c8f82dafa74f7608dfd062
U-BSD\351013 <351013@SHS-10D9C06DD.beaverton.k12.or.us>
2019-04-08 12:25:36 -0700

    Restructured some of the algortithm

--- redBlackTree/main.cpp
+++ redBlackTree/main.cpp
@@ -3,11 +3,6 @@
 
 using namespace std;
 
-bool isCase1(Node* node);
-bool isCase2(Node* node);
-bool isCase3(Node* node);
-bool isCase4(Node* node);
-bool isCase5(Node* node);
 void case1(Node* node);
 void case2(Node* node);
 void case3(Node* node);
@@ -20,7 +15,9 @@ int main() {
   return 0;
 }
 
-bool isCase1(Node* head, Node* node) {
+void testAll(
+	     
+void case1(Node* head, Node* node) {
   if(node == head) {
     return true;
   }
